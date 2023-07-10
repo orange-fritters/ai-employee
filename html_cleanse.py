3eab4ca7bf575661556924550e7af2bb3e9d9890
orange-fritters <mindongss@snu.ac.kr>
2023-07-10 21:54:05 +0900

    ✨ [Add] Add html cleansing code

---
+++ html_cleanse.py
@@ -0,0 +1,115 @@
+from bs4 import BeautifulSoup, Tag, NavigableString
+
+
+def has_subsection_style(tag):
+    if tag.has_attr('style'):
+        if 'overflow:hidden' in tag['style'] and 'width:635px' in tag['style']:
+            return True
+    return False
+
+
+def has_article_style(tag):
+    if tag.has_attr('style'):
+        if 'font-size:12.0pt' in tag['style'] and 'font-weight:bold' in tag['style'] and 'text-decoration:underline' in tag['style']:
+            return True
+    return False
+
+
+def cleanse(file_name: str):
+    with open(file_name, "r") as file:
+        content = file.read()
+    soup = BeautifulSoup(content, 'html.parser')
+
+    for tag in soup.find_all():
+        content = tag.string
+        if content is not None:
+            content = content.strip()
+            if content == '':
+                tag.extract()
+
+    for tag in soup.find_all(True, {'class': True}):
+        if tag.name == "table" and tag.get('border') == '1':
+            continue
+        del tag['class']
+
+    tags_with_target_style = soup.find_all(has_subsection_style)
+    for i, tag in enumerate(tags_with_target_style, start=1):
+        parent = tag.find_parent().find_parent()
+        new_div = soup.new_tag('div')
+        new_div['class'] = 'subsection'
+        parent.replace_with(new_div)
+        new_div.append(parent)
+
+    for div in soup.find_all('div', {'class': 'subsection'}):
+        div.parent.unwrap()
+
+    subsections = soup.find_all('div', {'class': 'subsection'})
+
+    for index, subsection in enumerate(subsections):
+        elems = []
+        next_subsection = subsections[index + 1] if index + 1 < len(subsections) else None
+
+        for sibling in list(subsection.next_siblings):
+            if sibling is next_subsection:
+                break
+            if isinstance(sibling, NavigableString):
+                continue
+            elems.append(sibling.extract())
+
+        # Create new div and append all extracted elements to it
+        new_div = soup.new_tag('div')  # Create a new div tag
+        new_div['class'] = 'section'
+        new_div.append(subsection.extract())
+
+        for e in elems:
+            new_div.append(e)
+
+        # Insert the new div after the subsection
+        soup.append(new_div)
+
+    tags_with_article_style = soup.find_all(has_article_style)
+    for article in tags_with_article_style:
+        article.parent.unwrap()
+
+    for section in soup.find_all('div', {'class': 'section'}):
+        articles = section.find_all(has_article_style)
+        for index, article in enumerate(articles):
+            elems = []
+            next_article = articles[index + 1] if index + 1 < len(articles) else None
+            for sibling in list(article.next_siblings):
+                if next_article is not None and sibling in next_article:
+                    break
+                if isinstance(sibling, NavigableString):
+                    continue
+                elems.append(sibling.extract())
+
+            # Create new div and append all extracted elements to it
+            new_div = soup.new_tag('div')  # Create a new div tag
+            new_div['class'] = 'article'
+            new_div.append(article.extract())
+
+            for e in elems:
+                new_div.append(e)
+
+            # Insert the new div after the subsection
+            section.append(new_div)
+
+    for tag in soup.find_all(True, {'style': True}):
+        del tag['style']
+
+    for tag in soup.find_all('table'):
+        # give table a border.
+        tag['border'] = '1'
+
+    # for tag with class article, first span inside it should be bolded
+    for tag in soup.find_all('div', {'class': 'article'}):
+        span = tag.find('span')
+        if span is not None:
+            span['style'] = 'font-weight:bold'
+
+    with open("file.html", "w") as file:
+        file.write(str(soup.prettify()))
+
+
+if __name__ == "__main__":
+    cleanse("data/first.html")
